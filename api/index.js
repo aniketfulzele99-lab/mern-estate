49cabd65839c1afd6cde53ee7d85fc67e8e7a8b2
Aniket <aniketfulzele99@gmail.com>
2025-08-27 00:36:22 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
+
+
+      //  const mongoose = require('mongoose');
+
+        const dbURI = 'mongodb://127.0.0.1:27017/meanstate'; // Replace 'your_database_name'
+
+        mongoose.connect(dbURI)
+          .then(() => console.log('MongoDB connected successfully'))
+          .catch(err => console.error('MongoDB connection error:', err));
+
 
 const app = express();
+
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
